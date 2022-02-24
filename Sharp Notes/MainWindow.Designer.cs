70d355a94b1bd2352891c72340d885de8a9244a0
Dan Banfield <dandbanfield@gmail.com>
2022-02-24 20:03:59 +0000

    Add project files.

---
+++ Sharp Notes/MainWindow.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace Sharp_Notes
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.divider1 = new System.Windows.Forms.Label();
+            this.encodingLabel = new System.Windows.Forms.Label();
+            this.unknownLabel1 = new System.Windows.Forms.Label();
+            this.divider2 = new System.Windows.Forms.Label();
+            this.zoomLabel = new System.Windows.Forms.Label();
+            this.divider3 = new System.Windows.Forms.Label();
+            this.lineColumnLabel = new System.Windows.Forms.Label();
+            this.divider4 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.noteTextBox.BackColor = System.Drawing.Color.White;
+            this.noteTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.noteTextBox.Font = new System.Drawing.Font("Source Sans Pro", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noteTextBox.Location = new System.Drawing.Point(0, 26);
+            this.noteTextBox.Multiline = true;
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.noteTextBox.Size = new System.Drawing.Size(1404, 662);
+            this.noteTextBox.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.White;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip.Size = new System.Drawing.Size(1404, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // divider1
+            // 
+            this.divider1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.divider1.AutoSize = true;
+            this.divider1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divider1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.divider1.Location = new System.Drawing.Point(1256, 690);
+            this.divider1.Name = "divider1";
+            this.divider1.Size = new System.Drawing.Size(11, 16);
+            this.divider1.TabIndex = 2;
+            this.divider1.Text = "|";
+            this.divider1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // encodingLabel
+            // 
+            this.encodingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.encodingLabel.AutoSize = true;
+            this.encodingLabel.Location = new System.Drawing.Point(1273, 693);
+            this.encodingLabel.Name = "encodingLabel";
+            this.encodingLabel.Size = new System.Drawing.Size(37, 13);
+            this.encodingLabel.TabIndex = 3;
+            this.encodingLabel.Text = "UTF-8";
+            // 
+            // unknownLabel1
+            // 
+            this.unknownLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.unknownLabel1.AutoSize = true;
+            this.unknownLabel1.Location = new System.Drawing.Point(1150, 693);
+            this.unknownLabel1.Name = "unknownLabel1";
+            this.unknownLabel1.Size = new System.Drawing.Size(87, 13);
+            this.unknownLabel1.TabIndex = 4;
+            this.unknownLabel1.Text = "Windows (CRLF)";
+            // 
+            // divider2
+            // 
+            this.divider2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.divider2.AutoSize = true;
+            this.divider2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divider2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.divider2.Location = new System.Drawing.Point(1133, 690);
+            this.divider2.Name = "divider2";
+            this.divider2.Size = new System.Drawing.Size(11, 16);
+            this.divider2.TabIndex = 5;
+            this.divider2.Text = "|";
+            this.divider2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // zoomLabel
+            // 
+            this.zoomLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.zoomLabel.AutoSize = true;
+            this.zoomLabel.Location = new System.Drawing.Point(1094, 693);
+            this.zoomLabel.Name = "zoomLabel";
+            this.zoomLabel.Size = new System.Drawing.Size(33, 13);
+            this.zoomLabel.TabIndex = 6;
+            this.zoomLabel.Text = "100%";
+            // 
+            // divider3
+            // 
+            this.divider3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.divider3.AutoSize = true;
+            this.divider3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divider3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.divider3.Location = new System.Drawing.Point(1077, 690);
+            this.divider3.Name = "divider3";
+            this.divider3.Size = new System.Drawing.Size(11, 16);
+            this.divider3.TabIndex = 7;
+            this.divider3.Text = "|";
+            this.divider3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lineColumnLabel
+            // 
+            this.lineColumnLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lineColumnLabel.AutoSize = true;
+            this.lineColumnLabel.Location = new System.Drawing.Point(960, 693);
+            this.lineColumnLabel.Name = "lineColumnLabel";
+            this.lineColumnLabel.Size = new System.Drawing.Size(58, 13);
+            this.lineColumnLabel.TabIndex = 8;
+            this.lineColumnLabel.Text = "Ln 1, Col 1";
+            // 
+            // divider4
+            // 
+            this.divider4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.divider4.AutoSize = true;
+            this.divider4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divider4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.divider4.Location = new System.Drawing.Point(943, 690);
+            this.divider4.Name = "divider4";
+            this.divider4.Size = new System.Drawing.Size(11, 16);
+            this.divider4.TabIndex = 9;
+            this.divider4.Text = "|";
+            this.divider4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1404, 711);
+            this.Controls.Add(this.divider4);
+            this.Controls.Add(this.lineColumnLabel);
+            this.Controls.Add(this.divider3);
+            this.Controls.Add(this.zoomLabel);
+            this.Controls.Add(this.divider2);
+            this.Controls.Add(this.unknownLabel1);
+            this.Controls.Add(this.encodingLabel);
+            this.Controls.Add(this.divider1);
+            this.Controls.Add(this.noteTextBox);
+            this.Controls.Add(this.menuStrip);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainWindow";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sharp Notes - Untitled";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label divider1;
+        private System.Windows.Forms.Label encodingLabel;
+        private System.Windows.Forms.Label unknownLabel1;
+        private System.Windows.Forms.Label divider2;
+        private System.Windows.Forms.Label zoomLabel;
+        private System.Windows.Forms.Label divider3;
+        private System.Windows.Forms.Label lineColumnLabel;
+        private System.Windows.Forms.Label divider4;
+    }
+}
+
